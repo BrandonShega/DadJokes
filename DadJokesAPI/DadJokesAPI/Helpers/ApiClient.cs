06d9fefe9b389bcdb700e474046c5234202e71eb
Brandon Shega <b.shega@gmail.com>
2020-04-01 14:23:25 -0400

    Change limit to 30 so we don't have to loop over pages since we only need 30

--- DadJokesAPI/DadJokesAPI/Helpers/ApiClient.cs
+++ DadJokesAPI/DadJokesAPI/Helpers/ApiClient.cs
@@ -29,47 +29,34 @@ namespace DadJokesAPI.Helpers
         public async Task<List<T>> Search<T>(string query)
         {
             var results = new List<T>();
-            var page = 1;
 
-            // Make consecutive calls since we may not get 30 on the first page
-            do
-            {
-			    var searchUrl = ApiUrl + "/search";
-			    var queryParams = new Dictionary<string, string>();
-			    var queryArray = new List<string>();
+		    var searchUrl = ApiUrl + "/search";
+		    var queryParams = new Dictionary<string, string>();
+		    var queryArray = new List<string>();
 
-			    // Check if query was supplied
-			    if (query != null) 
-			    {
-					queryParams["term"] = query;
-			    }
-			    queryParams["page"] = $"{page}";
+		    // Check if query was supplied
+		    if (query != null) 
+		    {
+				queryParams["term"] = query;
+		    }
+		    queryParams["limit"] = "30";
 
-                // Loop over each query pair and create query string
-			    foreach(KeyValuePair<string, string> entry in queryParams)
-			    {
-					queryArray.Add($"{entry.Key}={entry.Value}");
-			    }
+			// Loop over each query pair and create query string
+		    foreach(KeyValuePair<string, string> entry in queryParams)
+		    {
+				queryArray.Add($"{entry.Key}={entry.Value}");
+		    }
 
-			    searchUrl += $"?{string.Join("&", queryArray)}";
+		    searchUrl += $"?{string.Join("&", queryArray)}";
 
-                var response = await Client.GetStringAsync(searchUrl);
-                var searchResult = JsonConvert.DeserializeObject<SearchResult<T>>(response);
+			var response = await Client.GetStringAsync(searchUrl);
+			var searchResult = JsonConvert.DeserializeObject<SearchResult<T>>(response);
 
-                if (searchResult.Results.Any()) 
-		        {
-                    results.AddRange(searchResult.Results.ToList());
-				}
-
-                // Exit in the case that we already have at least 30 results
-                if (results.Count() >= 30) 
-		        {
-                    return results.Take(30).ToList();
-				}
-
-                page = (searchResult.NextPage != searchResult.CurrentPage) ? searchResult.NextPage : 1;
-            } while (page > 1);
-            return results;
+			if (searchResult.Results.Any()) 
+			{
+				results = searchResult.Results.ToList();
+			} 
+			return results;
 		}
     }
 }
